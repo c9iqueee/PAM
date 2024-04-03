e1d6d4b26cec4b68d06fa1cff7dddf82bcbe1223
Caique <caique.rocha@etec.sp.gov.br>
2024-04-02 22:14:18 -0300

    att-0204

---
+++ src/Components/Title/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {View, Text} from "react-native";
+
+export default function Title()
+{
+    return(
+        <View>
+            <Text>My first app</Text>
+        </View>
+    );
+}
